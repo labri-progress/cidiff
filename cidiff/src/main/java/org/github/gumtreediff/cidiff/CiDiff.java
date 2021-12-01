83cacfcd5c674ba36969a06ae702348634d31e8d
Jean-Rémy Falleri <jr.falleri@gmail.com>
2021-12-01 18:02:11 +0100

    fix: extract parseOptions method

--- cidiff/src/main/java/org/github/gumtreediff/cidiff/CiDiff.java
+++ cidiff/src/main/java/org/github/gumtreediff/cidiff/CiDiff.java
@@ -6,6 +6,11 @@ public class CiDiff {
     public static void main(String[] args) {
         final String leftLogFile = args[0];
         final String rightLogFile = args[1];
+        final Properties options = parseOptions(args);
+        final LogDiffer d = new LogDiffer(leftLogFile, rightLogFile, options);
+    }
+
+    private static Properties parseOptions(String args[]) {
         final Properties options = new Properties();
         if (args.length > 2) {
             if ((args.length - 2) % 3 != 0)
@@ -13,13 +18,13 @@ public class CiDiff {
             for (int i = 2; i < args.length; i = i + 3) {
                 final String option = args[i];
                 if (!option.equals("-o"))
-                    throw new IllegalArgumentException("Illegal option " + option);
+                    throw new IllegalArgumentException("Illegal option flag: " + option);
 
                 final String key = args[i + 1];
                 final String value = args[i + 2];
                 options.setProperty(key, value);
             }
         }
-        final LogDiffer d = new LogDiffer(leftLogFile, rightLogFile, options);
+        return options;
     }
 }
