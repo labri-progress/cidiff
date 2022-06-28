96777c5796b2763117794d44674bf8b9becdc47c
Jean-Rémy Falleri <jr.falleri@gmail.com>
2022-06-28 08:44:53 +0200

    fix: benchmark does not read groundtruth for each algorithm.

--- cidiff/src/main/java/org/github/gumtreediff/cidiff/benchmarks/PrecisionBenchmark.java
+++ cidiff/src/main/java/org/github/gumtreediff/cidiff/benchmarks/PrecisionBenchmark.java
@@ -1,20 +1,16 @@
 package org.github.gumtreediff.cidiff.benchmarks;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 
 import org.github.gumtreediff.cidiff.*;
 
 public final class PrecisionBenchmark {
-    private static final String HEADER = "LEFT;RIGHT;"
+    private static final String HEADER = "LEFT;"
+            + "RIGHT;"
             + "ALGORITHM;"
             + "PRECISION;"
             + "RECALL;"
@@ -26,7 +22,7 @@ public final class PrecisionBenchmark {
     private PrecisionBenchmark() {
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         if (args.length == 2) {
             pathToData = args[0];
             pathToOutput = args[1];
@@ -43,10 +39,11 @@ public final class PrecisionBenchmark {
             final String left = oneCaseFolder.toPath().resolve("pass.log").toString();
             final String right = oneCaseFolder.toPath().resolve("fail.log").toString();
             final String groundtruth = oneCaseFolder.toPath().resolve("groundtruth").toString();
-            final String parser = getParser(groundtruth);
+
+            final GroundtruthContent groundtruthContent = GroundtruthContent.fromFile(groundtruth);
 
             final Properties options = new Properties();
-            options.setProperty(Options.PARSER, parser);
+            options.setProperty(Options.PARSER, groundtruthContent.parser());
             final Pair<String> logs = new Pair<>(left, right);
 
             for (LogDiffer.Algorithm algorithm : LogDiffer.Algorithm.values()) {
@@ -59,12 +56,12 @@ public final class PrecisionBenchmark {
                 final Pair<Action[]> actions = differ.diff(lines);
                 final long stop = System.currentTimeMillis();
 
-                final List<int[]> intervalsCidiff = getIntervalsCidiff(actions);
-                final List<int[]> intervalsGroundtruth = getIntervalsGroundtruth(groundtruth, lines.right);
+                final List<int[]> intervalsCiDiff = getIntervalsCiDiff(actions.right, lines.right);
+                final List<int[]> intervalsGroundtruth = groundtruthContent.interval();
 
-                final float truePositive = getIntersections(intervalsCidiff, intervalsGroundtruth);
+                final float truePositive = getIntersections(intervalsCiDiff, intervalsGroundtruth);
                 final float falseNegative = getComplements(intervalsGroundtruth, truePositive);
-                final float falsePositive = getComplements(intervalsCidiff, truePositive);
+                final float falsePositive = getComplements(intervalsCiDiff, truePositive);
 
                 final float precision = truePositive / (truePositive + falsePositive);
                 final float recall = truePositive / (truePositive + falseNegative);
@@ -78,58 +75,24 @@ public final class PrecisionBenchmark {
         csv.close();
     }
 
-    private static String getParser(String groundtruth) throws Exception {
-        final BufferedReader br = new BufferedReader(new FileReader(groundtruth));
-        final String parser = br.readLine();
-        br.close();
-        return parser;
-    }
-
-    private static List<int[]> getIntervalsCidiff(Pair<Action[]> actions) {
+    private static List<int[]> getIntervalsCiDiff(Action[] rightActions, List<LogLine> rightLog) {
         int start = -1;
         final List<int[]> intervals = new ArrayList<>();
 
-        for (Action a: actions.right) {
-            if (a.type == Action.Type.ADDED && start == -1) {
-                start = a.rightLocation;
-            }
+        for (int i = 0; i < rightActions.length; i++) {
+            final Action a = rightActions[i];
+            if (a.type == Action.Type.ADDED && start == -1)
+                start = rightLog.get(i).lineNumber;
             else if (a.type != Action.Type.ADDED && start != -1) {
-                intervals.add(new int[]{start, a.rightLocation});
+                intervals.add(new int[]{start, rightLog.get(i).lineNumber});
                 start = -1;
             }
-            else if (a.type == Action.Type.ADDED && a.rightLocation == actions.right.length - 1) {
-                intervals.add(new int[] {start, a.rightLocation + 1});
+            else if (a.type == Action.Type.ADDED && i == rightActions.length - 1) {
+                intervals.add(new int[] {start, rightLog.get(i).lineNumber});
                 start = -1;
             }
         }
-        return intervals;
-    }
 
-    private static List<int[]> getIntervalsGroundtruth(String groundtruth, List<LogLine> lines) throws Exception {
-        final List<int[]> intervals = new ArrayList<>();
-        final BufferedReader br = new BufferedReader(new FileReader(groundtruth));
-        String oneLine;
-
-        final Map<Integer, Integer> logHashMap = new HashMap<Integer, Integer>();
-        for (int i = 0; i < lines.size(); i++) {
-            logHashMap.put(lines.get(i).lineNumber, i);
-        }
-
-        while ((oneLine = br.readLine()) != null) {
-            if (oneLine.charAt(0) == 'E' || oneLine.charAt(0) == 'C') {
-                final String[] errors = oneLine.substring(2).split("-");
-                final int l = Integer.parseInt(errors[0]);
-                final int r;
-                if (errors.length > 1) {
-                    r = Integer.parseInt(errors[1]);
-                }
-                else {
-                    r = l;
-                }
-                intervals.add(new int[] {logHashMap.get(l), logHashMap.get(r) + 1});
-            }
-        }
-        br.close();
         return intervals;
     }
 
@@ -142,9 +105,8 @@ public final class PrecisionBenchmark {
             final int l = Math.max(intervalsCidiff.get(i)[0], intervalsGroundtruth.get(j)[0]);
             final int r = Math.min(intervalsCidiff.get(i)[1], intervalsGroundtruth.get(j)[1]);
 
-            if (l <= r) {
+            if (l <= r)
                 truePositive += r - l;
-            }
 
             if (intervalsCidiff.get(i)[1] < intervalsGroundtruth.get(j)[1])
                 i += 1;
@@ -159,5 +121,28 @@ public final class PrecisionBenchmark {
         for (int[] r: intervals)
             complement += r[1] - r[0];
         return complement - truePositive;
+    }
+
+    private record GroundtruthContent(String parser, List<int[]> interval) {
+        static GroundtruthContent fromFile(String file) throws IOException {
+            final BufferedReader br = new BufferedReader(new FileReader(file));
+            final String parser = br.readLine();
+            final List<int[]> intervals = new ArrayList<>();
+            String oneLine;
+            while ((oneLine = br.readLine()) != null) {
+                if (oneLine.charAt(0) == 'E' || oneLine.charAt(0) == 'C') {
+                    final String[] errors = oneLine.substring(2).split("-");
+                    final int l = Integer.parseInt(errors[0]);
+                    final int r;
+                    if (errors.length > 1)
+                        r = Integer.parseInt(errors[1]);
+                    else
+                        r = l;
+                    intervals.add(new int[] {l, r + 1});
+                }
+            }
+            br.close();
+            return new GroundtruthContent(parser, intervals);
+        }
     }
 }
