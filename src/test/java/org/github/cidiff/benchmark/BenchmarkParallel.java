cdb2cb810789b48e39061133cf9bc470fc22ad01
Nicolas Hubner <nicolas.hubner@etu.u-bordeaux.fr>
2024-03-27 11:40:45 +0100

    rewrite code for benchmark

--- src/test/java/org/github/cidiff/benchmark/BenchmarkParallel.java
+++ src/test/java/org/github/cidiff/benchmark/BenchmarkParallel.java
@@ -18,24 +18,24 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Benchmark {
+public class BenchmarkParallel {
 
 	private static final Path DATASET = Path.of("data/breakages/");
 	private static final int LOOPS = 1;
 	private static final String SUCCESS_FILE = "pass.log";
 	private static final String FAILURE_FILE = "fail.log";
 
+	record Tuple2<A,B>(A a, B b) {}
+	record Tuple3<A,B,C>(A a, B b, C c) {}
+	record Tuple4<A,B,C,D>(A a, B b, C c, D d) {}
+	record Tuple5<A,B,C,D,E>(A a, B b, C c, D d, E e) {}
+	record Tuple6<A,B,C,D,E,F>(A a, B b, C c, D d, E e, F f) {}
+
 	public static void main(String[] args) throws IOException {
 		List<Path> directories = collectDirectories();
 
-		File file = new File("build/reports/benchmark.csv");
-		if (!file.getParentFile().exists() && !file.getParentFile().mkdirs()) {
-			throw new IllegalStateException("Cannot create directories for " + file.getParentFile().getAbsolutePath());
-		}
-		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-
-		writer.write("directory,type,duration,lines-left,lines-right,actions,added,deleted,updated,moved-unchanged,moved-updated,similar-groups,similar-groups-left,similar-groups-right\n");
 
 		LogParser parser = LogParser.Type.GITHUB.construct();
 		LogDiffer seed = LogDiffer.Algorithm.SEED.construct();
@@ -45,21 +45,35 @@ public class Benchmark {
 		Options optionsEven = new Options().with(Options.EVEN_IDENTICAL, true);
 		Options optionsLcs = new Options().with(Options.METRIC, Metric.EQUALITY);
 
-		for (int i = 0; i < directories.size(); i++) {
-			Path dir = directories.get(i);
-			List<Line> leftLines = parser.parse(dir.resolve(SUCCESS_FILE).toString(), options);
-			List<Line> rightLines = parser.parse(dir.resolve(FAILURE_FILE).toString(), options);
-			compute(i, directories.size(), "seed", dir, seed, leftLines, rightLines, options, writer);
-			compute(i, directories.size(), "seed-even", dir, seed, leftLines, rightLines, optionsEven, writer);
-			compute(i, directories.size(), "lcs", dir, lcs, leftLines, rightLines, optionsLcs, writer);
-		}
+		int s = directories.size() * 3;
+		AtomicInteger i = new AtomicInteger();
+		List<String> strs = directories.stream()
+				.parallel()
+				.map(dir -> new Tuple3<>(dir, parser.parse(dir.resolve(SUCCESS_FILE).toString(), options), parser.parse(dir.resolve(FAILURE_FILE).toString(), options)))
+				.map(tuple -> new Tuple4<>(tuple.a,
+						compute(i, s, "seed", tuple.a, seed, tuple.b, tuple.c, options),
+						compute(i, s, "seed-even", tuple.a, seed, tuple.b, tuple.c, optionsEven),
+						compute(i, s, "lcs", tuple.a, lcs, tuple.b, tuple.c, optionsLcs)
+				))
+				.map(tuple -> tuple.b + tuple.c + tuple.d)
+				.toList();
 
+		File file = new File("build/reports/benchmark.csv");
+		if (!file.getParentFile().exists() && !file.getParentFile().mkdirs()) {
+			throw new IllegalStateException("Cannot create directories for " + file.getParentFile().getAbsolutePath());
+		}
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+		writer.write("directory,type,duration,lines-left,lines-right,actions,added,deleted,updated,moved-unchanged,moved-updated,similar-groups,similar-groups-left,similar-groups-right\n");
+		for (String str : strs) {
+			writer.write(str);
+		}
 		writer.close();
 		System.out.println("done");
 	}
 
-	private static void compute(int i, int size, String type, Path dir, LogDiffer seed, List<Line> leftLines, List<Line> rightLines, Options options, BufferedWriter writer) throws IOException {
-		System.out.printf("%d/%d %s %s%n", i, size, type, DATASET.relativize(dir));
+	private static String compute(AtomicInteger i, int size, String type, Path dir, LogDiffer seed, List<Line> leftLines, List<Line> rightLines, Options options) {
+		System.out.printf("%d/%d %s %s%n", i.incrementAndGet(), size, type, DATASET.relativize(dir));
 		long duration = 0;
 		for (int loop = 0; loop < LOOPS; loop++) {
 			long b = System.currentTimeMillis();
@@ -69,13 +83,14 @@ public class Benchmark {
 		}
 		Pair<List<Action>> actions = seed.diff(leftLines, rightLines, options);
 		Metrics metrics = metric(actions);
-		writer.write("\"%s\",%s,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d%n".formatted(
+		String res = "\"%s\",%S,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d%n".formatted(
 				DATASET.relativize(dir).toString(), type, (duration / LOOPS),
 				leftLines.size(), rightLines.size(), metrics.actions, metrics.added, metrics.deleted,
 				metrics.updated, metrics.movedUnchanged, metrics.movedUpdated,
 				(metrics.similarBlockLeft + metrics.similarBlockRight), metrics.similarBlockLeft, metrics.similarBlockRight
-		));
+		);
 		System.gc();
+		return res;
 	}
 
 	private static Metrics metric(Pair<List<Action>> actions) {
