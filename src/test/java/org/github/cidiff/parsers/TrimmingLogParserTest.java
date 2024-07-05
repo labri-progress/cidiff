2cf50670a2f636531654f47e4ce385efc6bd8040
Nicolas hubner <nicolas.hubner@u-bordeaux.fr>
2024-07-05 16:48:30 +0200

    fix output of filtered client and trimming log parser test

--- src/test/java/org/github/cidiff/parsers/TrimmingLogParserTest.java
+++ src/test/java/org/github/cidiff/parsers/TrimmingLogParserTest.java
@@ -23,46 +23,31 @@ class TrimmingLogParserTest {
 		Options options = new Options();
 		TrimmingLogParser parserTrim0 = new TrimmingLogParser();
 		List<Line> parsed = parserTrim0.parse(TrimmingLogParserTest.class.getClassLoader().getResource("test-parser-trim.log").getPath(), options);
-		assertEquals(3, parsed.size());
+		assertEquals(2, parsed.size());
 		assertEquals("line 1", parsed.get(0).raw());
 		assertEquals("line 1", parsed.get(0).value());
 		assertEquals(0, parsed.get(0).index());
-		assertEquals("", parsed.get(1).raw());
-		assertEquals("", parsed.get(1).value());
+		assertEquals("line 2", parsed.get(1).raw());
+		assertEquals("line 2", parsed.get(1).value());
 		assertEquals(1, parsed.get(1).index());
-		assertEquals("line 2", parsed.get(2).raw());
-		assertEquals("line 2", parsed.get(2).value());
-		assertEquals(2, parsed.get(2).index());
 
 		// trim 2 characters
 		Options options1 = new Options().with(Options.DEFAULT_TRIM, 2);
 		TrimmingLogParser parserTrim2 = new TrimmingLogParser();
 		List<Line> parsed1 = parserTrim2.parse(TrimmingLogParserTest.class.getClassLoader().getResource("test-parser-trim.log").getPath(), options1);
-		assertEquals(3, parsed1.size());
+		assertEquals(2, parsed1.size());
 		assertEquals("line 1", parsed1.get(0).raw());
 		assertEquals("ne 1", parsed1.get(0).value());
 		assertEquals(0, parsed1.get(0).index());
-		assertEquals("", parsed.get(1).raw());
-		assertEquals("", parsed.get(1).value());
-		assertEquals(1, parsed.get(1).index());
-		assertEquals("line 2", parsed1.get(2).raw());
-		assertEquals("ne 2", parsed1.get(2).value());
-		assertEquals(2, parsed1.get(2).index());
+		assertEquals("line 2", parsed1.get(1).raw());
+		assertEquals("ne 2", parsed1.get(1).value());
+		assertEquals(1, parsed1.get(1).index());
 
 
 		// trim all characters
 		Options options2 = new Options().with(Options.DEFAULT_TRIM, 50);
 		TrimmingLogParser parserTrim50 = new TrimmingLogParser();
 		List<Line> parsed2 = parserTrim50.parse(TrimmingLogParserTest.class.getClassLoader().getResource("test-parser-trim.log").getPath(), options2);
-		assertEquals(3, parsed2.size());
-		assertEquals("line 1", parsed2.get(0).raw());
-		assertEquals("", parsed2.get(0).value());
-		assertEquals(0, parsed2.get(0).index());
-		assertEquals("", parsed2.get(1).raw());
-		assertEquals("", parsed2.get(1).value());
-		assertEquals(1, parsed2.get(1).index());
-		assertEquals("line 2", parsed2.get(2).raw());
-		assertEquals("", parsed2.get(2).value());
-		assertEquals(2, parsed2.get(2).index());
+		assertEquals(0, parsed2.size());
 	}
 }
